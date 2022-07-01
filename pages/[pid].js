53a6d75a26fdadcc18c81206372890d6f5d835b2
colinmfoster4723 <colin.foster4723@gmail.com>
2022-06-30 19:52:36 -0500

    Initial Commit

---
+++ pages/[pid].js
@@ -0,0 +1,48 @@
+import fs from "fs/promises";
+import path from "path";
+
+import { Fragment } from "react";
+
+function ProductDetailPage(props) {
+  const { loadedProduct } = props;
+
+  return (
+    <Fragment>
+      <h1>{loadedProduct.title}</h1>
+      <p>{loadedProduct.description}</p>
+    </Fragment>
+  );
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+  //object of key/values with concreate dynamic segments//
+  const productId = params.pid;
+
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  const product = data.products.find((product) => product.id === productId);
+
+  return {
+    props: {
+      loadedProduct: product,
+    },
+  };
+}
+//context holds concreate values for dynamic segments//
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: { pid: "p1" } },
+      { params: { pid: "p2" } },
+      { params: { pid: "p3" } },
+    ],
+    fallback: false,
+  };
+}
+//tell nextjs which instances of dynamic page should be pre-generated
+
+export default ProductDetailPage;
